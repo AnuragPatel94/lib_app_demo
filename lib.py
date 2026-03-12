cf17b4f4cde32af8914247b418e60c85c70c39d0
Anurag <apatel665@gmail.com>
2026-03-12 16:09:55 +0530

    print action remove

--- lib.py
+++ lib.py
@@ -82,10 +82,5 @@ if __name__ == '__main__':
             "author": f"Author {i}",
             "isbn": f"ISBN-{i:05d}" 
         })
-        print(f"Added book: {books[-1]}")
-        
-        
-
-    
-    app.run(debug=True)
+        app.run(debug=True)
 
