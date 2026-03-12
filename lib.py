0b6dc9cb6bbe3178a91d9e4a3b588918eefb43db
Anurag <apatel665@gmail.com>
2026-03-12 15:52:16 +0530

    docker file update

--- lib.py
+++ lib.py
@@ -35,7 +35,6 @@ def delete_book(book_id):
 # Update a book
 @app.route('/books/<int:book_id>', methods=['PUT'])
 def update_book(book_id):
-    global books
     for book in books:
         if book["id"] == book_id:
             data = request.get_json()
@@ -83,7 +82,7 @@ if __name__ == '__main__':
             "author": f"Author {i}",
             "isbn": f"ISBN-{i:05d}" 
         })
-        print(f"Added book {i}")
+        
 
     
     app.run(debug=True)
